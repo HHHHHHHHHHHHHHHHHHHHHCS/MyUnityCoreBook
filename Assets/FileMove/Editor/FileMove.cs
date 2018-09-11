d2a225409b8af68ae4a16483fe5d9ff7fcd5718c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-12 00:48:28 +0800

    Dont move file

---
+++ Assets/FileMove/Editor/FileMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FileMove : UnityEditor.AssetModificationProcessor
+{
+    private static string prefabPath = "Assets/FileMove/TestPrefab.prefab";
+
+    public static AssetMoveResult OnWillMoveAsset(string oldPath,string newPath)
+    {
+        AssetMoveResult result = AssetMoveResult.DidNotMove;
+        if(oldPath==prefabPath)
+        {
+            result = AssetMoveResult.FailedMove;
+            EditorUtility.DisplayDialog("Error", "Don't Move Me!","OK!!!");
+        }
+        return result;
+    }
+}
