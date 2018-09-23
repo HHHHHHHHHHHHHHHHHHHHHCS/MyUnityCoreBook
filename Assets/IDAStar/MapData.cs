d2f66d34c1420ab16ab79e4021c3e750067c0bee
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-24 01:06:02 +0800

    Astar

---
+++ Assets/IDAStar/MapData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData
+{
+    public const int width = 6;
+    public const int height = 6;
+    public static readonly int[,] pathMap = new int[,]{
+        {0,0,0,0,-1,0 },
+        {0,0,0,0,-1,0 },
+        {0,0,0,0,-1,0 },
+        {0,-1,-1,-1,-1,0 },
+        {0,0,0,-1,0,0 },
+        {0,-1,0,0,0,0 }};
+}
+
+public class SearchNode
+{
+    public Vector2 pos;
+    public float f;
+    public float g;
+    public float h;
+    public SearchNode parent;
+
+    public SearchNode(Vector2 pos)
+    {
+        this.pos = pos;
+        parent = null;
+    }
+}
