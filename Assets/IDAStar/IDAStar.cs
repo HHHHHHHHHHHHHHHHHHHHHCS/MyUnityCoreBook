c5e9f35eea1171daa96683b5f5cc3539a84d92bf
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-25 01:28:54 +0800

    astar

---
+++ Assets/IDAStar/IDAStar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IDAStar : MonoBehaviour
+{
+    private void FindWay(SearchNode startNode,SearchNode endNode)
+    {
+        List<SearchNode> openSet = new List<SearchNode>();
+        List<SearchNode> closeSet = new List<SearchNode>();
+
+        SearchNode currentNode = startNode;
+
+        while(currentNode.IsEqual(endNode))
+        {
+            List<SearchNode> adjacentNode = currentNode.GetAdjacent();
+            for(int i=0;i<adjacentNode.Count;i++)
+            {
+
+            }
+
+            if(openSet.Count==0)
+            {
+                break;
+            }
+
+            //TODO:
+        }
+    }
+}
--- Assets/IDAStar/MapData.cs
+++ Assets/IDAStar/MapData.cs
@@ -28,4 +28,71 @@ public class SearchNode
         this.pos = pos;
         parent = null;
     }
+
+    public bool IsEqual(SearchNode node2)
+    {
+        return NodeMethod.IsEqual(this, node2);
+    }
+
+    public List<SearchNode> GetAdjacent()
+    {
+        return NodeMethod.GetAdjacent(this);
+    }
+
+    public bool IsValidPos()
+    {
+        return NodeMethod.IsValidPos(pos);
+    }
+}
+
+public static class NodeMethod
+{
+
+    public static bool IsEqual(SearchNode node1, SearchNode node2)
+    {
+        return node1.pos.x == node2.pos.x && node1.pos.y == node2.pos.y;
+    }
+
+    public static List<SearchNode> GetAdjacent(SearchNode node)
+    {
+        var pos = node.pos;
+        Vector2 up, down, left, right;
+        up = new Vector2(pos.x, pos.y - 1);
+        down = new Vector2(pos.x, pos.y + 1);
+        left = new Vector2(pos.x - 1, pos.y);
+        right = new Vector2(pos.x + 1, pos.y);
+
+        List<SearchNode> nodeList = new List<SearchNode>()
+        {
+            Capacity=4
+        };
+        if(IsValidPos(up))
+        {
+            nodeList.Add(new SearchNode(up));
+        }
+        if (IsValidPos(down))
+        {
+            nodeList.Add(new SearchNode(down));
+        }
+        if (IsValidPos(left))
+        {
+            nodeList.Add(new SearchNode(left));
+        }
+        if (IsValidPos(right))
+        {
+            nodeList.Add(new SearchNode(right));
+        }
+
+        return nodeList;
+    }
+
+    public static bool IsValidPos(Vector2 pos)
+    {
+        if(pos.x<0||pos.x>MapData.width-1
+            || pos.y < 0 || pos.y > MapData.height - 1)
+        {
+            return false;
+        }
+        return MapData.pathMap[(int)pos.x, (int)pos.y] == 0 ? true : false;
+    }
 }
