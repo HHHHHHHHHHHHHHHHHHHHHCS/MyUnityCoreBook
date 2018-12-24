d54628bd2e2a15c336b7b371fc005247c3678d49
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-12-25 00:37:15 +0800

    Runback

---
+++ Assets/RunBackground/Editor/AutoServer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Unity点击 Play的时候脚本会rebuild一次
+/// 所以需要闭环服务
+/// </summary>
+[InitializeOnLoad]
+public class AutoServer
+{
+
+    static AutoServer()
+    {
+        return;
+        if(!EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            EditorApplication.update += ScriptLoaod;
+        }
+        else
+        {
+            EditorApplication.update += ScriptRunning;
+        }
+
+    }
+
+    private static void ScriptLoaod()
+    {
+        EditorApplication.update -= ScriptLoaod;
+        EditorApplication.update += IdleUpdate;
+    }
+
+    private static void IdleUpdate()
+    {
+        CheckAndShow();
+        if(EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            EditorApplication.update -= IdleUpdate;
+        }
+    }
+
+    private static void ScriptRunning()
+    {
+        EditorApplication.update -= ScriptRunning;
+        EditorApplication.update += RunningUpdate;
+    }
+
+    private static void RunningUpdate()
+    {
+        CheckAndShow();
+        if (!EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            EditorApplication.update -= RunningUpdate;
+            EditorApplication.update += ScriptRunning;
+        }
+    }
+
+    private static void CheckAndShow()
+    {
+        var now = System.DateTime.Now;
+        if(now.Hour==17&&now.Minute==0&&now.Second==0)
+        {
+            if (EditorUtility.DisplayDialog("吃饭啦!!!", "五点钟该吃饭了"
+                ,"走起","算球!"))
+            {
+                Application.OpenURL("https://www.baidu.com/");
+            }
+
+        }
+    }
+}
